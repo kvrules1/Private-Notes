93120f222db0dd23d00b5da8ed9dfefe9fe068b4
Kenneth McDonald <kennethvnelson@gmail.com>
2020-09-23 13:51:42 -0400

    Initial Commit

---
+++ Private Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Private Notes
+//
+//  Created by KV on 9/23/20.
+//  Copyright © 2020 Kenneth McDonald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
