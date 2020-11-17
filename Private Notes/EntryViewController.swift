aad54a9359460d174baa9c91b53abe76a003f773
Kenneth McDonald <kennethvmcdonald@knights.ucf.edu>
2020-11-17 17:03:26 -0500

    new

---
+++ Private Notes/EntryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryViewController.swift
+//  Private Notes
+//
+//  Created by KV on 9/23/20.
+//  Copyright © 2020 Kenneth McDonald. All rights reserved.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+    
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var noteField: UITextView!
+
+    public var completion: ((String, String) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleField.becomeFirstResponder()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
+    }
+    
+    @objc func didTapSave(){
+        if let text = titleField.text, !text.isEmpty, !noteField.text.isEmpty{
+            completion?(text, noteField.text)
+        }
+    }
+
+}
