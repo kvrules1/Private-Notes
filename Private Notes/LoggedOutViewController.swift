4df01e11ae1219a3dbe91e2abc47caaf67ab2fa8
Kenneth McDonald <kennethvmcdonald@knights.ucf.edu>
2020-11-18 15:35:18 -0500

    implemented some sorting features and logout feature

---
+++ Private Notes/LoggedOutViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoggedOutViewController.swift
+//  Private Notes
+//
+//  Created by KV on 11/18/20.
+//  Copyright © 2020 Kenneth McDonald. All rights reserved.
+//
+
+import Foundation
--- Private Notes/ViewController.swift
+++ Private Notes/ViewController.swift
@@ -14,14 +14,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet var table: UITableView!
     @IBOutlet var label: UILabel!
+    @IBOutlet weak var editButton: UIBarButtonItem!
     
-    var models: [(title: String, note: String)] = []
+    var models: [(title: String, note: String, date: Date)] = []
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         table.delegate = self
         table.dataSource = self
         title = "Notes"
+        
     }
     
     @IBAction func didTapNewNote(){
@@ -32,13 +35,26 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         vc.navigationItem.largeTitleDisplayMode = .never
         vc.completion = { noteTitle, note in
             self.navigationController?.popToRootViewController(animated: true)
-            self.models.append((title: noteTitle, note: note))
+            self.models.append((title: noteTitle, note: note, date: Date()))
             self.label.isHidden = true
             self.table.isHidden = false
             
             self.table.reloadData()
         }
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func logOutAction(sender: AnyObject) {
+        if Auth.auth().currentUser != nil {
+            do {
+                try Auth.auth().signOut()
+                //let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Login")
+                //present(vc, animated: true, completion: nil)
+                self.performSegue(withIdentifier: "Logout", sender: nil)
+            } catch let error as NSError {
+                print(error.localizedDescription)
+                }
+        }
     }
     
     // Table Functions
@@ -52,6 +68,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = models[indexPath.row].title
         cell.detailTextLabel?.text = models[indexPath.row].note
         return cell
+    }
+    // Allows for reorder
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool
+    {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath)
+    {
+        let item = models[sourceIndexPath.row]
+        models.remove(at: sourceIndexPath.row)
+        models.insert(item, at: destinationIndexPath.row)
+    }
+    
+    @IBAction func sortAlpha(_ sender: Any)
+    {
+        self.models.sort { $0.title < $1.title }
+        self.table.reloadData()
+    }
+    
+    @IBAction func sortDate(_ sender: Any)
+    {
+        self.models.sort { $0.date < $1.date }
+        self.table.reloadData()
+    }
+    
+    @IBAction func edit(_ sender: Any)
+    {
+        table.isEditing = !table.isEditing
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete{
+            models.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+        }
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -73,4 +125,3 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
 }
-
